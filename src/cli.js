083aad6dacd071a3cdbeaf44ee8c751ea50df66d
Анастасия Левчук <nastya@MacBook-Air-Anastasia.local>
2022-10-07 16:15:03 +0300

    add eslint

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
+
 const greetingUser = () => {
-  var userName = readlineSync.question("May I have your name? ");
-  console.log("Hello, " + userName + "!");
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
 
 export default greetingUser;
