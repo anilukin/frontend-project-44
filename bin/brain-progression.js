0ab4a84ff20bfd56e305a0634b4771492b848a7b
Анастасия Левчук <nastya@MacBook-Air-Anastasia.local>
2022-10-17 00:51:07 +0300

    fix progression

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,49 +1,7 @@
 #!/usr/bin/env node
 
-import {
-  printWelcome, getUserName, greetUser, questionCount, randomNumber, getUsersAnswer, accept,
-  reject, congr,
-} from '../src/index.js';
+import { game } from '../src/index.js';
 
-import { progressionTask } from '../src/games/progression.js';
+import { progressionTask, progressionGame } from '../src/games/progression.js';
 
-printWelcome();
-const userName = getUserName();
-greetUser(userName);
-
-progressionTask();
-
-let mistake = false;
-
-for (let i = 0; i < questionCount; i += 1) {
-  const progrLength = randomNumber(5, 10);
-  const firstProgrElement = randomNumber(0, 50);
-  const progrStep = randomNumber(0, 50);
-
-  const progression = [];
-
-  for (let j = 0; j < progrLength; j += 1) {
-    const element = firstProgrElement + j * progrStep;
-    progression.push(element);
-  }
-
-  const questionElement = randomNumber(0, (progrLength - 1));
-
-  const rightAnswer = progression[questionElement];
-
-  progression[questionElement] = '..';
-
-  console.log(`Question: ${progression.join(' ')}`);
-
-  const usersAnswer = getUsersAnswer();
-  if (usersAnswer === rightAnswer.toString()) {
-    accept();
-  } else {
-    reject(usersAnswer, rightAnswer, userName);
-    mistake = true;
-    break;
-  }
-}
-if (mistake === false) {
-  congr(userName);
-}
+game(progressionTask, progressionGame);
--- src/games/progression.js
+++ src/games/progression.js
@@ -1,5 +1,34 @@
+import { randomNumber, getUsersAnswer } from '../index.js';
+
 const progressionTask = () => console.log('What number is missing in the progression?');
 
+const progressionGame = () => {
+  const progrLength = randomNumber(5, 10);
+  const firstProgrElement = randomNumber(0, 50);
+  const progrStep = randomNumber(0, 50);
+
+  const progression = [];
+
+  for (let i = 0; i < progrLength; i += 1) {
+    const element = firstProgrElement + i * progrStep;
+    progression.push(element);
+  }
+
+  const questionElement = randomNumber(0, (progrLength - 1));
+
+  const rightAnswer = progression[questionElement];
+
+  progression[questionElement] = '..';
+
+  console.log(`Question: ${progression.join(' ')}`);
+
+  const usersAnswer = getUsersAnswer();
+
+  const answerArray = [rightAnswer.toString(), usersAnswer];
+
+  return answerArray;
+};
+
 export {
-    progressionTask,
+  progressionTask, progressionGame,
 };
