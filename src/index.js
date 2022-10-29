86bfa3d5064310def2673dfab4080929bd552723
Анастасия Левчук <nastya@MacBook-Air-Anastasia.local>
2022-10-29 15:57:11 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -11,27 +11,21 @@ const game = (description, getAnswerAndQuestion) => {
 
   console.log(description);
 
-  let mistake = false;
-
   for (let i = 0; i < questionCount; i += 1) {
-    const answerAndQuestionArr = getAnswerAndQuestion();
+    const [question, rightAnswer] = getAnswerAndQuestion();
 
-    console.log(`Question: ${answerAndQuestionArr[0]}`);
+    console.log(`Question: ${question}`);
 
     const usersAnswer = readlineSync.question('Your answer: ');
-    const rightAnswer = answerAndQuestionArr[1];
 
     if (usersAnswer === rightAnswer) {
       console.log('Correct!');
     } else {
       console.log(`'${usersAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`);
-      mistake = true;
-      break;
+      return;
     }
   }
-  if (mistake === false) {
-    console.log(`Congratulations, ${userName}!`);
-  }
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default game;
